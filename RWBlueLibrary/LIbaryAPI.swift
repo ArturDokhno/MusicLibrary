7bd5605c0cd721a1039ee154ed7393035ca3384e
ArturDokhno <artur.dohno@icloud.com>
2021-12-20 19:25:07 +0500

    Правка

---
+++ RWBlueLibrary/LIbaryAPI.swift
@@ -0,0 +1,19 @@
+//
+//  LIbaryAPI.swift
+//  RWBlueLibrary
+//
+//  Created by Артур Дохно on 20.12.2021.
+//  Copyright © 2021 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+final class LibraryAPI {
+  
+  static let shared = LibraryAPI()
+  
+  private init() {
+
+  }
+  
+}
