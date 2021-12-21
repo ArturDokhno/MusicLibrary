9df69d5c776f10db2d9af36a52563e4425e3f1f4
ArturDokhno <artur.dohno@icloud.com>
2021-12-21 21:28:00 +0500

    Паттерн Наблюдатель ( KVO-Key-Value Observing )

--- RWBlueLibrary/AlbumView.swift
+++ RWBlueLibrary/AlbumView.swift
@@ -14,6 +14,8 @@ class AlbumView: UIView {
   private var coverImageView: UIImageView!
   private var indicatorView: UIActivityIndicatorView!
   
+  private var valueObservation: NSKeyValueObservation!
+  
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     commonInit()
@@ -31,6 +33,13 @@ class AlbumView: UIView {
     // Создайте представление изображения обложки
     coverImageView = UIImageView()
     coverImageView.translatesAutoresizingMaskIntoConstraints = false
+    
+    valueObservation = coverImageView.observe(\.image, options: [.new]) { [unowned self] observed, change in
+      if change.newValue is UIImage {
+          self.indicatorView.stopAnimating()
+      }
+    }
+    
     addSubview(coverImageView)
     // Создайте представление индикатора
     indicatorView = UIActivityIndicatorView()
